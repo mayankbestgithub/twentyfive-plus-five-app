e5f440487baabdcceb8c3ef63215162da7b212d8
mayank <mayank.thapliyal1@gmail.com>
2024-03-19 14:02:50 +0530

    made ui and css adjustments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>25+5 Clock</h1>
+      <audio id="beep">
+        <source src="" />
+      </audio>
+      <div>
+        <span id="break-label">
+
+          <strong>Break Length</strong>
+          <div>
+            <button id="break-decrement">&#8595;</button><span id="break-length">5</span><button id="break-increment">&#8593;</button>
+          </div>
+        </span>
+        <span id="session-label">
+
+          <strong>Session Length</strong>
+          <div>
+            <button id="session-decrement">&#8595;</button><span id="session-length">25</span><button id="session-increment">&#8593;</button>
+          </div>
+        </span>
+        <div id="timer-label">
+          <span>Session</span>
+          <div id="time-left">25:00</div>
+          <div><button id="start_stop" >&#9658;</button><button>&#x23E9;&#xFE0E;</button><button id="reset">&#10561;</button></div>
+        </div>
+      </div>
+    </div >
   );
 }
 
